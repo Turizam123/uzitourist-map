2df86053a5ba6990ac66b9993bc6481b4558797b
Turizam123 <nssr@abv.bg>
2025-07-28 23:39:57 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('tourist-cache').then(cache => {
+      return cache.addAll(['.', 'index.html', 'manifest.json']);
+    })
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
